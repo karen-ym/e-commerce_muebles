c2ddd13d170750f31abe26f8f0ed96e3648a7a52
Karen <karenymorel@gmail.com>
2024-10-20 14:50:33 -0300

    Añadí entidades: categoría, producto, proveedor

---
+++ e-commerce_muebles.Managers/Entidades/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce_muebles.Managers.Entidades
+{
+    public class Producto
+    {
+        public int id_producto { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public double precio { get; set; }
+        public int stock { get; set; }
+    }
+}
---
+++ e-commerce_muebles.Managers/Entidades/Proveedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce_muebles.Managers.Entidades
+{
+    public class Proveedor
+    {
+        public int id_proveedor { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
+        public int direccion_id { get; set; }
+    }
+}
