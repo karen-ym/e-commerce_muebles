3cc71c2feb80b0571ab217f3ecf58a761d83d285
Karen <karenymorel@gmail.com>
2024-10-20 14:08:57 -0300

    Carpetas

---
+++ e-commerce_muebles.Managers/Entidades/Auditoria/Audit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce_muebles.Managers.Entidades.Auditoria
+{
+    internal class Audit
+    {
+        // completar luego
+    }
+}
