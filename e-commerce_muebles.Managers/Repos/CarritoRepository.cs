8a9e05eba93f731fbc7f95eeea59b3f6f9596db9
JorgePomaV <jpomavergara1418@gmail.com>
2024-10-21 16:31:47 -0300

    se agrego repositorio

---
+++ e-commerce_muebles.Managers/Repos/CarritoRepository.cs
@@ -0,0 +1,75 @@
+﻿using e_commerce_muebles.Managers.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace e_commerce_muebles.Managers.Repos
+{
+    public interface ICarritoRepository
+    {
+        bool AgregarProducto(int id_producto, int id_cliente, int cantidad);
+        bool EliminarProducto(int id_producto, int id_cliente);
+        IEnumerable<Carrito> GetCarrito(int id_cliente);
+        bool EditarCantidadProducto(int id_cliente, int id_procuto, int cantidad);
+    }
+    public class CarritoRepository : ICarritoRepository
+    {
+        private string _connectionString;
+        public CarritoRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public bool AgregarProducto(int producto_id, int cliente_id, int cantidad)
+        {
+            using (IDbConnection conn = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO CARRITO (producto_id, cliente_id, cantidad) VALUES (@id_producto, @id_cliente, @cant)";
+                var resultado = conn.Execute(query, new { id_producto = producto_id, id_cliente = cliente_id, cant = cantidad });
+                return resultado == 1;
+            }
+        }
+
+        public bool EditarCantidadProducto(int id_cliente, int id_procuto, int cantidad)
+        {
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                if (cantidad > 0)
+                {
+                    string query = "UPDATE CARRITO SET cantidad = @Cantidad WHERE producto_id = @Id_producto AND cliente_id = @Id_cliente";
+                    var resultado = connection.Execute(query, new { Cantidad = cantidad, Id_producto = id_procuto, Id_cliente = id_cliente });
+                    return resultado == 1;
+                }
+                else
+                {
+                    this.EliminarProducto(id_procuto, id_cliente);
+                }
+                return false;
+            }
+        }
+
+        public bool EliminarProducto(int id_producto, int id_cliente)
+        {
+            using (IDbConnection conn = new SqlConnection(_connectionString))
+            {
+                string query = "DELETE FROM CARRITO WHERE producto_id = @Id_producto AND cliente_id = @Id_cliente";
+                var resultado = conn.Execute(query, new { Id_producto = id_producto, Id_cliente = id_cliente });
+                return resultado == 1;
+            }
+        }
+
+        public IEnumerable<Carrito> GetCarrito(int id_cliente)
+        {
+            using (IDbConnection conn = new SqlConnection(_connectionString))
+            {
+                IEnumerable<Carrito> carritos = conn.Query<Carrito>("SELECT * FROM CARRITO WHERE cliente_id = @Id_cliente", new { Id_cliente = id_cliente }).ToList();
+                return carritos;
+            }
+        }
+    }
+}
---
+++ e-commerce_muebles.Managers/Repos/DireccionRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using e_commerce_muebles.Managers.Entidades;
+
+namespace e_commerce_muebles.Managers.Repos
+{
+    internal class DireccionRepository
+    {
+        public interface IDireccionRepository
+        {
+            bool AgregarDireccion(Direccion direccion);
+            bool EditarDireccion(Direccion direccion);
+            Direccion GetDireccion(int id_direccion);
+            bool BorrarDireccion(int id_cliente);
+        }
+        public class DireccionRepository : IDireccionRepository
+        {
+            private string _ConnectionString;
+            public DireccionRepository(string connectionString)
+            {
+                _ConnectionString = connectionString;
+            }
+            public bool AgregarDireccion(Direccion direccion)
+            {
+                using (IDbConnection con = new SqlConnection(_ConnectionString))
+                {
+                    string query = "ISERT INTO DIRECCION (id_direccion, calle, altura, departamento, barrio, localidad) VALUES (@id_direccion, @calle, @altura, @departamoento, @barrio, @localidad)";
+                    var resultado = con.Execute(query, direccion);
+                    return resultado == 1;
+                }
+            }
+
+            public bool BorrarDireccion(int id_direccion)
+            {
+                using (IDbConnection conn = new SqlConnection(_ConnectionString))
+                {
+                    string query = "DELETE FROM DIRECCION WHERE id_direccion = @Id_Direcion";
+                    var resultado = conn.Execute(query, new { Id_direccin = id_direccion });
+                    return resultado == 1;
+                }
+
+
+
+            }
+
+            public bool EditarDireccion(Direccion direccion)
+            {
+                using (IDbConnection conn = new SqlConnection(_ConnectionString))
+                {
+                    string query = "UPDATE DIRECCION SET calle = @calle, altura = @altura, departamento = @departamento, barrio = @barrio, localidad = @localidad WHERE id_direccion = @id_direccion";
+                    var resultado = conn.Execute(query, direccion);
+                    return resultado == 1;
+                }
+            }
+
+            public Direccion GetDireccion(int id_direccion)
+            {
+                using (IDbConnection conn = new SqlConnection(_ConnectionString))
+                {
+                    Direccion direccion = conn.QuerySingle<DIRECCION>("SELECT * FROM DIRECCION WHERE id_direccion = @Id_direccion", new { Id_direccion = id_direccion });
+                    return direccion;
+                }
+            }
+        }
+    }
+}
