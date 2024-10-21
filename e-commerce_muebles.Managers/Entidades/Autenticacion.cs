06822fd38134be44840a0f0ea4aa43f31d0b25c2
David vasquez <vasquezdavid325@gmail.com>
2024-10-20 21:54:58 -0300

    Se agrego Entidades

---
+++ e-commerce_muebles.Managers/Entidades/Autenticacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce_muebles.Managers
+{
+    public class Autenticacion
+    {
+        public int IdAtenticacion { get; set; }
+        public string Email { get; set; }
+        public String Clave { get; set; }
+        public int UsuarioId { get; set; }
+
+    }
+}
---
+++ e-commerce_muebles.Managers/Entidades/Historial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce_muebles.Managers.Entidades
+{
+    public class Historial
+    {
+        public int IdHistorial { get; set; }
+        public int UsuarioId { get; set; }
+        public int VentaId { get; set; }
+
+    }
+}
